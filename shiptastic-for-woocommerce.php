a4fd0a7a32f6359086a7d4d9178fc6cde7708ac3
vendidero <info@vendidero.de>
2025-08-01 11:23:18 +0200

    Bump version to 4.5.5. Disable translation override for standalone installation.

--- shiptastic-for-woocommerce.php
+++ shiptastic-for-woocommerce.php
@@ -6,7 +6,7 @@
  * Author: vendidero
  * Author URI: https://vendidero.com
  * Text Domain: shiptastic-for-woocommerce
- * Version: 4.5.4
+ * Version: 4.5.5
  * Requires at least: 5.4
  * Requires PHP: 5.6
  * License: GPLv3
