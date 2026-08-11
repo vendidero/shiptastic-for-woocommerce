8dfcf53f7c403059294dbbea6ee3f3be25dad1d0
vendidero <info@vendidero.de>
2026-08-11 09:14:56 +0200

    Adding /build directory to release

---
+++ build/static/admin-shipping-provider-method.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5fc0cdffad1193fd5d68');
---
+++ build/static/pickup-locations.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7ab4ed485c7f8f9e8a2a');
